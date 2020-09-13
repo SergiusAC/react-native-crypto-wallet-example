bf025fb121cc4d542037ce5cb54e157a18ca1c54
Sergey Chin <the.sergiusac@gmail.com>
2020-09-13 23:44:58 +0600

    add file

--- src/modules/wallet/components/AccountItem/index.js
+++ src/modules/wallet/components/AccountItem/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { View, Text, Card, CardItem, Body } from 'native-base';
+import { View, Text, Card, CardItem, Body, Spinner } from 'native-base';
+import { getBalanceInEther } from '../../../../services/ether';
 
 const AccountItem = (props) => {
+
+  const [balance, setBalance] = React.useState(null);
+
+  getBalanceInEther(props.account.address)
+    .then(res => {
+      setBalance(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  const spinnerElement = <Spinner size="small" style={{height: 'auto'}} />;
+  const balanceElement = <Text>{balance} ETH</Text>
+
+  const amount =  balance === null ? spinnerElement : balanceElement;
+
   return (
     <CardItem key={props.account.address} bordered button onPress={() => props.onShowAccount(props.account)}>
       <Body>
@@ -9,6 +26,9 @@ const AccountItem = (props) => {
           <View style={{flex: 2}}>
             <Text>Имя: {props.account.name}</Text>
             <Text>Адрес: {props.account.address.substr(0, 14)}...</Text>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            {amount}
           </View>
         </View>
       </Body>
